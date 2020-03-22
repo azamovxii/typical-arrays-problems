fa1c66a790407389eda4308caee94c2e1a58d1d0
otabekazamov <54863708+otabekazamov@users.noreply.github.com>
2020-03-22 23:02:29 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
+const isArrayInvalid = (array) => {
+  return (!array || !array.length);
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = function min(array) {
+  return (isArrayInvalid(array)) ? 0 : Math.min(...array);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+  return (isArrayInvalid(array)) ? 0 : Math.max(...array);
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+  return (isArrayInvalid(array)) ? 0 : 
+    array.reduce((previousItem, currentItem) => {
+      return (previousItem + currentItem);
+    }, 0) / array.length;
+};
